bfd0866bcba97bb5eabd12acc987ecc00665f680
Cam Way <cway140@gmail.com>
2022-09-20 14:39:01 -0700

    Initial Commit

---
+++ Whistle Timer/Whistle_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whistle_TimerApp.swift
+//  Whistle Timer
+//
+//  Created by Cam Way on 2022-09-20.
+//
+
+import SwiftUI
+
+@main
+struct Whistle_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
